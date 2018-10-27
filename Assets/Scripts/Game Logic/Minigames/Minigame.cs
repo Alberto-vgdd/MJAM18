b953fce0459e1277ade232ed63fafffab365bb4e
Alberto <alberto.vgdd@gmail.com>
2018-10-27 10:13:32 +0200

    Modified InputManager and created materials.

---
+++ Assets/Scripts/Game Logic/Minigames/Minigame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Minigame : MonoBehaviour
+{
+    public abstract void MinigameStart();
+    public abstract void MinigamePause();
+    public abstract void MinigameResume();
+    public abstract void MinigameStop();
+
+}
+
+public abstract class Minigame<T> : Minigame where T : Minigame<T>
+{
+    public static T Instance { get; private set; }
+
+    public virtual void Awake()
+    {
+        Instance = (T)this;
+    }
+}
