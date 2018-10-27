da7da67200c0fbe431d0dd7706a7ba23e8c16e0f
Slava <superm30@hotmail.com>
2018-10-27 12:56:26 +0200

    Horse Luis

--- Assets/Scripts/Horse Minigame/HorseMovement.cs
+++ Assets/Scripts/Horse Minigame/HorseMovement.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class HorseMovement : MonoBehaviour
 {
+
+    public GameObject horse;
+    public float velocidadHorse;
+    public float amplitudHorse;
+
+    public float numAvancePlayer1;
+    public float numRetrocesoPlayer2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
 	{
-		if(){}
+        horse.transform.position += new Vector3(0, Mathf.Sin(Time.time) * velocidadHorse * amplitudHorse, 0);
+
+        if (InputManager.GetActionDown(InputManager.Player.One)) {
+            horse.transform.position += new Vector3(numAvancePlayer1, 0, 0);
+        }
+
+        if (InputManager.GetActionDown(InputManager.Player.Two))
+        {
+            horse.transform.position += new Vector3(-numRetrocesoPlayer2, 0, 0);
+        }
     }
 }
