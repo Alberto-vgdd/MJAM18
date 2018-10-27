42e3230b471a48dec42d8796a5584ecb5f2f506c
Slava <superm30@hotmail.com>
2018-10-27 14:07:29 +0200

    Horse Script y Pong Script

--- Assets/Scripts/Horse Minigame/HorseMovement.cs
+++ Assets/Scripts/Horse Minigame/HorseMovement.cs
@@ -11,28 +11,34 @@ public class HorseMovement : MonoBehaviour
 
     public float numAvancePlayer1;
     public float numRetrocesoPlayer2;
-    public bool action0;
-    public bool action1;
+
+    private Vector3 posicionInicialHorse;
 
     // Start is called before the first frame update
     void Start()
     {
+        posicionInicialHorse = horse.transform.position;
     }
 
     // Update is called once per frame
     void Update()
 	{
-        action0 = InputManager.GetAction(Player.One);
-        action1 = InputManager.GetAction(Player.Two);
-        //horse.transform.position += new Vector3(0, Mathf.Sin(Time.time) * velocidadHorse * amplitudHorse, 0);
+        horse.transform.position += new Vector3(0, Mathf.Sin(Time.time) * velocidadHorse * amplitudHorse, 0);
 
-        if (action0) {
+        if (InputManager.GetActionDown(Player.One)) {
             horse.transform.position += new Vector3(numAvancePlayer1, 0, 0);
         }
 
-        if (action1)
+        if (InputManager.GetActionDown(Player.Two) && horse.transform.position.x >= posicionInicialHorse.x)
         {
             horse.transform.position -= new Vector3(numRetrocesoPlayer2, 0, 0);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player") {
+            //ACTIVAR TRANSICION CAMBIO NIVEL
         }
     }
 }
